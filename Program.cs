4d77b808669c1bd20e99b820dca32751211796e4
Iryna_Luchyn <luchyn.ira@gmail.com>
2026-01-26 18:26:51 +0200

    Startup migration

--- Program.cs
+++ Program.cs
@@ -4,37 +4,35 @@ using IncidentServiceAPI.Middleware;
 using IncidentServiceAPI.Services;
 using IncidentServiceAPI.Data;
 
-
 var builder = WebApplication.CreateBuilder(args);
 
-
-
-// ----------------------------
-// 1. Add services to DI
-// ----------------------------
-
-// Controllers
 builder.Services.AddControllers();
-
-// DbContext with SQL Server
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Services
 builder.Services.AddScoped<IIncidentService, IncidentService>();
 builder.Services.AddScoped<IAccountService, AccountService>();
 
-// Swagger/OpenAPI
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// ----------------------------
-// 2. Middleware pipeline
-// ----------------------------
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<AppDbContext>();
+        context.Database.Migrate();
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred while migrating the database.");
+    }
+}
 
-// Global exception handling
 app.UseMiddleware<ExceptionHandlingMiddleware>();
 
 if (app.Environment.IsDevelopment())
@@ -44,9 +42,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
